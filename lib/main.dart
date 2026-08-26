235c4f557aafdce56406f80df502472c88609ed9
Kimdog-LAB <114891355+Kimdog-LAB@users.noreply.github.com>
2026-08-26 17:14:34 +0000

    Connect Firebase project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 import 'money_app.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } catch (_) {
+    // Firebase 연결 실패해도 앱은 계속 실행 (로그인 기능만 비활성)
+  }
   runApp(const MoneyApp());
 }
